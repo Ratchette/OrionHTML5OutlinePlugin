82820e706086db53cc221db2d873f745dc1db25f
Jennifer Winer <jenn.winer@gmail.com>
2013-09-30 16:11:17 -0400

    The plugin that I am attempting to create is so similar to the existing HTML one that I finally broke down and will use the HTML plugin as a basis for my work.I believe that after integrating our two works it still meets the requirements of the EPL

--- htmlOutliner.js
+++ htmlOutliner.js
@@ -1,87 +1,182 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *	   Jennifer Winer - Extension to HTML5 Outlining
+ *******************************************************************************/
+
+/*global Tautologistics eclipse self */
+
+//This is a copy of the HTML outline plugin coded as a web worker plugin
+
 window.onload = function(){
-	function parseHTML(contents){
+	function parse(contents) {
+		var domResult;
 		var handler = new Tautologistics.NodeHtmlParser.HtmlBuilder(
-			function (error, dom){
-				if(error)
-					return null;
-			},	
+			function(error, dom) {
+				if (!error) {
+					//parsing done
+					domResult = dom;
+				}
+			},
+			
 			{includeLocation: true, ignoreWhitespace: true}
 		);
-			
+
 		var parser = new Tautologistics.NodeHtmlParser.Parser(handler);
 		parser.parseComplete(contents);
-		return handler.dom;
+		return domResult;
 	}
-
-	var headers = {
-		name: "HTML5 Outliner Plugin",
-		version: "0.1",
-		desrciption: "Produces an HTML5 outline for a document"
-	};
-	var serviceProperties = {
-		contentType: ["text/html"],
-		name: "HTML5 Outline",
-		id: "orion.outliner.html5.outliner"
-	};
 	
-	var serviceImpl = {
-		getOutline: function(contents, title){
-			var outline = [];
-			var dom = parseHTML(document.getElementsByTagName('html')[0].innerHTML);
-					
-			// the algorithm stuff
-					
-			var outlineTarget = null;
-			var section = null;
-			var stack = [];	
-			
-			var root = null;
-		
-			dom.forEach(function(child){
-				if(child.name === "body"){
-					root = child;
-				}
-			});
-			
-			if(root === null){
-				root = dom;
+	/**
+	 * Returns whether this HTML node should be omitted from the outline.
+	 * @param {Object} node The HTML element
+	 * @param {Boolean} filter if true the tree should be filtered to only show most relevant entries
+	 * @return {boolean} true if the element should be skipped, and false otherwise
+	 */
+	function skip(node, filter) {
+		//skip nodes with no name
+		if (!node.name) {
+			return true;
+		}
+	
+		//if user wants a full tree, do no further filtering
+		if (!filter) {
+			return false;
+		}
+		//skip formatting elements
+		if (node.name === "b" || node.name === "i" || node.name === "em") {
+			return true;
+		}
+	
+		//skip paragraphs and other blocks of formatted text
+		if (node.name === "p" || node.name === "tt" || node.name === "code" || node.name === "blockquote") {
+			return true;
+		}
+	
+		//skip anchors
+		if (node.name === "a") {
+			return true;
+		}
+	
+		//include the element if we have no reason to skip it
+		return false;
+	}
+	
+	/**
+	 * Converts an HTML dom element into a label
+	 * @param {Object} element The HTML element
+	 * @return {String} A human readable label
+	 */
+	
+	function domToLabel(node) {
+		var label = node.name;
+		//include id if present
+		var match = /id=['"]\S*["']/.exec(node.raw);
+		if (match) {
+			label = label + " " + match[0];
+		}
+		//include class if present
+		match = /class=['"]\S*["']/.exec(node.raw);
+		if (match) {
+			label = label + " " + match[0];
+		}
+		return label;
+	}
+	
+	/**
+	 * Converts an HTML DOM node into an outline element
+	 * @param {Object} An HTML DOM node as returned by the Tautologistics HTML parser
+	 * @param {Boolean} filter if true the tree should be filtered to only show most relevant entries
+	 * @return {Object} A node in the outline tree
+	 */
+	
+	function domToOutline(dom, filter) {
+		//end recursion
+		if (!dom) {
+			return null;
+		}
+		var outline = [];
+		for (var i = 0; i < dom.length; i++) {
+			var node = dom[i];
+			if (!skip(node, filter)) {
+				var element = {
+					label: domToLabel(node),
+					children: domToOutline(node.children, filter),
+					line: node.location.line
+				};
+				outline.push(element);
 			}
-					
-			return outline;
+		}
+		return outline;
+	}
+	
+	/**
+	 * Returns the DOM node corresponding to the HTML body, 
+	 * or null if no such node could be found.
+	 */
+	function findBody(dom) {
+		//recursively walk the dom looking for a body element
+		for (var i = 0; i < dom.length; i++) {
+			if (dom[i].name === "body") {
+				return dom[i].children;
+			}
+			if (dom[i].children) {
+				var result = findBody(dom[i].children);
+				if (result) {
+					return result;
+				}
+			}
+		}
+		return null;
+	}
+	
+	function buildOutline(contents, title, filter) {
+		var dom = parse(contents);
+		if (!dom) {
+			return null;
+		}
+		if (filter) {
+			//only process the document body
+			var body = findBody(dom);
+			if (body) {
+				dom = body;
+			}
+		}
+		var outline = domToOutline(dom, filter);
+		return outline;
+	}
+	
+	var outlineService = {
+		getOutline: function(contents, title) {
+			return buildOutline(contents, title, false);
 		}
 	};
 	
+	
+	/**
+	 * Create the plugin
+	 */
+	var headers = {
+		name: "HTML5 Outliner Plugin",
+		version: "0.2",
+		desrciption: "Produces an HTML5 outline for a document"
+	};
+	
+	var serviceProperties = {
+		contentType: ["text/html"],
+		pattern: "\\.html?$",
+		name: "HTML5 Outline",
+		id: "orion.edit.outliner.html5"
+	};
+	
 	var provider = new eclipse.PluginProvider(headers);
-	provider.registerServiceProvider("orion.edit.outliner", serviceImpl, serviceProperties);
+	provider.registerServiceProvider("orion.edit.outliner", outlineService, serviceProperties);
 	provider.connect();
 };
-
-
-// the algorithm that I should be following
-/*
-function (root, enter, exit) {
-  var node = root;
-  
-  start: while (node) {
-    enter(node);
-    
-    if (node.firstChild) {
-      node = node.firstChild;
-      continue start;
-    }
-    
-    while (node) {
-      exit(node);
-      if (node === root) {
-        node = null;
-      } else if (node.nextSibling) {
-        node = node.nextSibling;
-        continue start;
-      } else {
-        node = node.parentNode;
-      }
-    }
-  }
-}
-
-*/
